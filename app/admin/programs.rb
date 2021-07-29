c0f9adc419d2442e1ab970316da5c1359469aeac
Anton Naumenkov <a.naumenkov@mobidev.biz>
2021-07-29 18:20:54 +0300

    active admin feature installation

---
+++ app/admin/programs.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Program do
+  permit_params :name, :description
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Program" do
+      f.input :name
+      f.input :description
+    end
+    f.actions
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'home#main'
 
   get '/sign_in', to: 'sessions#sign_in'
