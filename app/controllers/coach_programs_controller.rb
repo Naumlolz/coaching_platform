dbf2fcf065fc82fb9e476c6ccdf2470405846b25
Антон Науменков <NaumE64@yandex.ru>
2025-03-26 10:49:11 +0300

    fix: eager loading refactoring for coaches programs

--- app/controllers/coach_programs_controller.rb
+++ app/controllers/coach_programs_controller.rb
@@ -1,6 +1,6 @@
 # list of current coach programs
 class CoachProgramsController < ApplicationController
   def index
-    @coach_programs = CoachesProgram.where(coach_id: current_coach.id)
+    @coach_programs = CoachesProgram.where(coach_id: current_coach.id).includes(program: [:techniques])
   end
 end
